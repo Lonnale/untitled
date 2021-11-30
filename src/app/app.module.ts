2f61d757405f78f36a00c791ef717606b3333447
Aleksi <a.lonnrot@gmail.com>
2021-11-30 13:31:18 +0200

    väliosa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TrainingComponent} from './training/training.component';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {CalculatorComponent} from './calculator/calculator.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {SliderComponent} from './slider/slider.component';
+import {MatSliderModule} from '@angular/material/slider';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ReactiveformComponent} from './reactiveform/reactiveform.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {CinemaComponent} from './cinema/cinema.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainingComponent,
+    CalculatorComponent,
+    SliderComponent,
+    ToolbarComponent,
+    ReactiveformComponent,
+    CinemaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatGridListModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
